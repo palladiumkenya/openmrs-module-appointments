856dbd7e33723fc693f39d18817e51ca9fab8bbb
makombe <kennedymakombe@gmail.com>
2024-05-09 08:41:49 +0300

    Improve mark appointment as completd or Missed

--- api/src/main/java/org/openmrs/module/appointments/scheduler/tasks/MarkAppointmentAsCompleteTask.java
+++ api/src/main/java/org/openmrs/module/appointments/scheduler/tasks/MarkAppointmentAsCompleteTask.java
@@ -1,13 +1,21 @@
 package org.openmrs.module.appointments.scheduler.tasks;
 
 import org.openmrs.GlobalProperty;
+import org.openmrs.Patient;
+import org.openmrs.Visit;
 import org.openmrs.api.AdministrationService;
+import org.openmrs.api.VisitService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.appointments.model.Appointment;
+import org.openmrs.module.appointments.model.AppointmentSearchRequest;
 import org.openmrs.module.appointments.model.AppointmentStatus;
 import org.openmrs.module.appointments.service.AppointmentsService;
+import org.openmrs.module.appointments.util.DateUtil;
 import org.openmrs.scheduler.tasks.AbstractTask;
 
+import liquibase.pro.packaged.v;
+
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,7 +26,8 @@ public class MarkAppointmentAsCompleteTask extends AbstractTask {
     public void execute() {
         AppointmentsService appointmentsService = Context.getService(AppointmentsService.class);
         AdministrationService administrationService = Context.getService(AdministrationService.class);
-        GlobalProperty schedulerMarksCompleteProperty = administrationService.getGlobalPropertyObject("SchedulerMarksComplete");
+        GlobalProperty schedulerMarksCompleteProperty = administrationService
+                .getGlobalPropertyObject("SchedulerMarksComplete");
         Boolean schedulerMarksComplete = Boolean.valueOf(schedulerMarksCompleteProperty.getPropertyValue());
 
         if (!schedulerMarksComplete) {
@@ -26,18 +35,34 @@ public class MarkAppointmentAsCompleteTask extends AbstractTask {
         }
 
         Date today = new Date();
-        List<Appointment> appointments = appointmentsService.getAllAppointmentsInDateRange(null, today);
-        List<Appointment> scheduledAndCheckedInAppointments = appointments.stream()
-                .filter(appointment -> isAppointmentCheckedIn(appointment))
-                .collect(Collectors.toList());
-        for (Appointment appointment : scheduledAndCheckedInAppointments) {
-            String status = AppointmentStatus.Completed.toString();
-            appointmentsService.changeStatus(appointment, status, today);
+        AppointmentSearchRequest appointmentSearchRequest = new AppointmentSearchRequest();
+        appointmentSearchRequest.setStatus(AppointmentStatus.CheckedIn);
+        appointmentSearchRequest.setStartDate(DateUtil.getStartOfDay());
+        appointmentSearchRequest.setEndDate(DateUtil.getEndOfDay());
+        List<Appointment> appointments = appointmentsService.search(appointmentSearchRequest);
+
+        for (Appointment appointment : appointments) {
+            if (isTodaysVisitCheckedOut(appointment.getPatient())) {
+                String status = AppointmentStatus.Completed.toString();
+                appointmentsService.changeStatus(appointment, status, today);
+
+            }
+
         }
     }
 
     private boolean isAppointmentCheckedIn(Appointment appointment) {
         return appointment.getStatus().equals(AppointmentStatus.CheckedIn);
+    }
+
+    private boolean isTodaysVisitCheckedOut(Patient patient) {
+        Date startOfDay = DateUtil.getStartOfDay();
+        Date endOfDay = DateUtil.getEndOfDay();
+        VisitService visitService = Context.getVisitService();
+        List<Visit> visits = visitService.getVisits(null, Arrays.asList(patient), null, null, startOfDay, null, null,
+                endOfDay, null, true, false);
+        return visits.size() > 0;
+
     }
 
 }
